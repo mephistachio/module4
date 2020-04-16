f88ac8266c88ef0475c11dde05be03182c7acba2
Maksym Godunov <Maksym_Godunov@epam.com>
2020-04-16 10:40:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+
+namespace module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Please enter the text: ");
+			StringBuilder stringbuilder = new StringBuilder();
+			otherMain();
+			try
+			{
+				while (true)
+				{
+					var str = Console.ReadLine();
+					if (string.IsNullOrWhiteSpace(str))
+					{
+						throw new NullReferenceException("NullReferenceException: string is empty");
+					}
+					if (str.IndexOf("-quit", StringComparison.Ordinal) != -1)
+					{
+						break;
+					}
+
+					_ = stringbuilder.Append(str[0]);
+					_ = stringbuilder.Append(" ");
+				}
+
+			}
+			catch (NullReferenceException nullEx)
+			{
+				Console.WriteLine("NullReferenceException:" + nullEx.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Result: ");
+				Console.WriteLine(stringbuilder.ToString());
+				Console.ReadKey();
+			}
+		}
+
+		static void otherMain()
+		{
+			var str = Console.ReadLine();
+			var t = str[0];
+		}
+	}
+}
+        
---
+++ test/test.module4.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using module4;
+using System.Threading.Tasks;
+
+
+namespace module4.test
+{
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Check2Strings()
+        { 
+
+        }
+    }
+}
